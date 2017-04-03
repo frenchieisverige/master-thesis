488791892b2190d4d168dc902096a6acbd7515fd
frenchieisverige <e0schwartz@gmail.com>
2017-04-03 10:12:15 +0200

    Final push before deadline & clean-up!

--- src/company/abi.js
+++
@@ -1,3 +0,0 @@
-var abiArray = 
-
-[{"constant":false,"inputs":[{"name":"x","type":"string"}],"name":"set","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"storedHash","type":"string"}],"payable":false,"type":"function"}];
